0110e0568a84a0796ea3719251ab2d417a7e909e
Mirzac Mihai <mihaimarzac@gmail.com>
2022-10-17 09:36:20 +0300

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: LandingPageComponent},
+  {path: 'menu', component: MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

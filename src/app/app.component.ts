55eae7277941a5059e8a128ff6428d2fd6abaf48
Mirzac Mihai <mihaimarzac@gmail.com>
2022-10-19 19:29:22 +0300

    Added services and responsive sidenav logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MenuService } from './services/menu.service';
+import { CartService } from './services/cart.service';
+import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
+import { ToggleSidenavService } from './components/header/toggle-sidenav.service';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { sanitizeIdentifier } from '@angular/compiler';
 
+const SMALL_WIDTH_BREAKPOINT = 45; //45em = 700px
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pizza_pasta';
+export class AppComponent implements OnInit, OnDestroy {
+  cartServiceSub$!: Subscription;
+  breakpointObs$!: Subscription;
+  sideNavSub$!: Subscription;
+
+  showSidenav!: boolean;
+  isSmall?: boolean;
+  cart: any[] = [];
+
+  navigationLinks: { link: string; icon: string }[] = [
+    { link: 'home', icon: 'home' },
+    { link: 'menu', icon: 'book' },
+    { link: 'cart', icon: 'shopping_cart' },
+    { link: 'contact', icon: 'contacts' },
+  ];
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  constructor(
+    private toggleSidenavService: ToggleSidenavService,
+    private cartService: CartService,
+    private menuService: MenuService,
+    private breakpointObs: BreakpointObserver,
+    private renderer: Renderer2,
+    private elementRef: ElementRef
+  ) {}
+
+  ngOnInit(): void {
+   this.cartServiceSub$ = this.cartService._cartSubject$.subscribe((cartItems) => {
+      this.cart = cartItems;
+      console.log(this.cart);
+    });
+    this.sideNavSub$ = this.toggleSidenavService.toggled.subscribe((res) => {
+      this.showSidenav = res;
+      console.log(res);
+    });
+
+    this.menuService._menuSubject$.subscribe(
+      res=>{
+        console.log(res);
+      }
+   )
+
+    this.breakpointObs$ = this.breakpointObs
+      .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}em)`])
+      .subscribe((state: BreakpointState) => {
+        console.log(state);
+        this.isSmall = state.matches;
+
+        if (!state.matches) {
+          this.showSidenav = true;
+          this.toggleSidenavService.toggled.next(this.showSidenav);
+        } else {
+          this.showSidenav = false;
+          this.toggleSidenavService.toggled.next(this.showSidenav);
+        }
+      });
+
+    let lastKnownScrollPosition = 0;
+    let ticking = false;
+
+    document.addEventListener('scroll', (e) => {
+      lastKnownScrollPosition = window.scrollY;
+
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          this.doSomething(lastKnownScrollPosition);
+          ticking = false;
+        });
+
+        ticking = true;
+      }
+    });
+  }
+  doSomething(scrollPos: any) {
+    // Do something with the scroll position
+    let navigation = this.sidenav._content.nativeElement.children[0];
+    this.renderer.setStyle(navigation, 'translate', `0px ${scrollPos}px`);
+    //console.log();
+  }
+  ngOnDestroy(): void {
+    this.breakpointObs$.unsubscribe();
+    this.cartServiceSub$.unsubscribe();
+    this.sideNavSub$.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TeamMemberComponent } from './components/team-member/team-member.component';
+import { MenuShowcaseItemComponent } from './components/menu-showcase-item/menu-showcase-item.component';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { WizardComponent } from './components/wizard/wizard.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CartComponent } from './components/cart/cart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    TeamMemberComponent,
+    MenuShowcaseItemComponent,
+    DialogComponent,
+    WizardComponent,
+    ContactComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

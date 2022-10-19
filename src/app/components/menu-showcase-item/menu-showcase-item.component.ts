cbe2fe1319031086ccef830542605b71bab4a42d
Mirzac Mihai <mihaimarzac@gmail.com>
2022-10-19 19:32:50 +0300

    Added menu-showcase component

---
+++ src/app/components/menu-showcase-item/menu-showcase-item.component.ts
@@ -0,0 +1,40 @@
+import { CartService } from './../../services/cart.service';
+
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-showcase-item',
+  templateUrl: './menu-showcase-item.component.html',
+  styleUrls: ['./menu-showcase-item.component.scss']
+})
+export class MenuShowcaseItemComponent implements OnInit {
+  @Input() menuItem!: any;
+  @Input() id!: any;
+  addedToCart?: boolean;
+  cart: any[] = [];
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+
+    this.cart = this.cartService.getCart();
+    if(this.cart.includes(this.menuItem)){
+      this.addedToCart = true;
+    } else {
+      this.addedToCart = false;
+    }
+  }
+
+  onAddToCart(item: any){
+    let items = this.cartService.getCart();
+
+    if(items.includes(item)){
+      this.addedToCart = false;
+      return;
+    } else {
+      this.addedToCart = true;
+       this.cartService.addItemToCart(item);
+    }
+   
+  }
+
+}

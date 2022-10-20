b33cc60e0db72247134f44a1fa27d1d17f1fb3c5
Mirzac Mihai <mihaimarzac@gmail.com>
2022-10-20 09:00:55 +0300

    Added delivery ETA in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { DeliveryService } from './../../services/delivery.service';
 import { ToggleSidenavService } from './toggle-sidenav.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private toggleSidenavService: ToggleSidenavService) { }
+  constructor(private toggleSidenavService: ToggleSidenavService, private deliveryService: DeliveryService) { }
   
   ngOnInit(): void {
+    this.deliveryService.getWeather().subscribe(
+      (res: any)=>{
+        let currentTimeGMT = res["current_weather"]["time"];
+        let actualCurrentTime = res["hourly"]["time"].indexOf(currentTimeGMT) + 3;
+        let actualTemp = res["hourly"]["temperature_2m"][actualCurrentTime];
+        let precipitation = res["hourly"]["precipitation"][actualCurrentTime];
+        console.log(actualCurrentTime);
+        console.log(actualTemp);
+        console.log(precipitation);
+        console.log(res['current_weather']);
+
+        this.ETA = this.deliveryService.returnDeliveryETA(actualTemp, precipitation);
+      }
+    )
   }
+  ETA?: number;
   isToggled = false;
   
   onToggle(){

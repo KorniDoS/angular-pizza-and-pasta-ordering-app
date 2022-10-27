40c68ad08e6ac52ce1bd837c8484fe11d32d6a2b
Mirzac Mihai <mihaimarzac@gmail.com>
2022-10-27 15:35:02 +0300

    Added paginator

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,43 +1,60 @@
 import { Pizza } from './../../models/pizza.model';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormArray,
-  FormControl,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { DialogComponent } from './../dialog/dialog.component';
 import { MenuService } from './../../services/menu.service';
-import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-import { MatTable } from '@angular/material/table';
-import { Inject } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  OnDestroy,
+  AfterViewInit,
+} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 import {
   combineLatest,
-  forkJoin,
   map,
-  mergeMap,
   of,
-  share,
   shareReplay,
   Subscription,
   switchMap,
 } from 'rxjs';
 import { Pasta } from 'src/app/models/pasta.model';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit, OnDestroy {
+export class MenuComponent implements OnInit, AfterViewInit, OnDestroy {
   menuItems: any[] = [];
   pizzaItems: Pizza[] = [];
   pastaItems: Pasta[] = [];
 
   wizardMode?: boolean = false;
   pizzaPastaSub$!: Subscription;
-  //secondStep: AbstractControlLike;
+
+  pizzaItemsDataSource = new MatTableDataSource<Pizza>();
+  pastaItemsDataSource = new MatTableDataSource<Pasta>();
+
+  @ViewChild('pizzaPaginator')
+  pizzaPaginator!: MatPaginator;
+  @ViewChild('pastaPaginator')
+  pastaPaginator!: MatPaginator;
+
+  newItemType?: string;
+  newToppingsArr: string[] = [];
+  newToppingsArrPasta: string[] = [];
+  pizzaColumns: string[] = ['position', 'image', 'name', 'toppings', 'price'];
+  pastaColumns: string[] = [
+    'position',
+    'image',
+    'name',
+    'ingredients',
+    'price',
+  ];
+
   constructor(
     private menuService: MenuService,
     public dialog: MatDialog,
@@ -59,49 +76,28 @@ export class MenuComponent implements OnInit, OnDestroy {
         this.pizzaItems = res[0];
         this.pastaItems = res[1];
 
-        console.log('New pasta items!');
-        console.log('New pizza items!');
+        this.pizzaItemsDataSource.data = this.pizzaItems;
+        this.pastaItemsDataSource.data = this.pastaItems;
       });
   }
-
-  newItemType?: string;
-  newToppingsArr: string[] = [];
-  newToppingsArrPasta: string[] = [];
-  pizzaColumns: string[] = ['position', 'image', 'name', 'toppings', 'price'];
-  pastaColumns: string[] = [
-    'position',
-    'image',
-    'name',
-    'ingredients',
-    'price',
-  ];
-  //dataSource?: any;
-
-  @ViewChild('pizzaTable') pizzaTable!: MatTable<any>;
-  @ViewChild('pastaTable') pastaTable!: MatTable<any>;
 
   createNew() {
     this.wizardMode = !this.wizardMode;
   }
-
-  removeData() {}
+  ngAfterViewInit(): void {
+    this.pizzaItemsDataSource.paginator = this.pizzaPaginator;
+    this.pastaItemsDataSource.paginator = this.pastaPaginator;
+  }
 
   onSelectItem(item: Pizza | Pasta, id: number) {
     console.log(item);
-    let dialogRef = this.dialog
-      .open(DialogComponent, {
-        minHeight: '500px',
-        maxHeight: '650px',
-        width: '80vw',
-        maxWidth: '600px',
-        data: { item: item, id: id },
-      })
-      .afterClosed()
-      .subscribe((ok) => {
-        this.pizzaTable.renderRows();
-        this.pastaTable.renderRows();
-        console.log('Rendered rows!');
-      });
+    let dialogRef = this.dialog.open(DialogComponent, {
+      minHeight: '500px',
+      maxHeight: '650px',
+      width: '80vw',
+      maxWidth: '600px',
+      data: { item: item, id: id },
+    });
 
     console.log(dialogRef);
   }
